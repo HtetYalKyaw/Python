c83f21385ef4289578e56dcfe19dd910abb2ad56
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-04-04 13:28:30 +0630

    test

--- input.py
+++ input.py
@@ -1,3 +1,6 @@
 name = input("What is your Name ? :")
+age = int(input("How old are you ? : "))
 
-print("Hello, Your Name is "+str(name))
+age = age + 1
+print("Hello, Your Name is " + name)
+print("You are " +str(age) + " years old.")
