3cf9289b4f2c80423ceeaab266d1251b17ce0006
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-04-04 13:52:09 +0630

    math

---
+++ Math function.py
@@ -0,0 +1,15 @@
+import math
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+# print(round(pi)) whole integer
+# print(math.ceil(pi)) highest
+# print(math.floor(pi)) lowest
+# print(abs(pi)) absolute value
+# print(pow(pi,3)) power
+# print((math.sqrt(pi))) square root
+# print(max(x,y,z)) find the max value
+# print(min(x,y,z )) find the min value
+
--- input.py
+++ input.py
@@ -1,6 +1,8 @@
 name = input("What is your Name ? :")
 age = int(input("How old are you ? : "))
+height = float(input("How Tall are you ?"))
 
 age = age + 1
 print("Hello, Your Name is " + name)
 print("You are " +str(age) + " years old.")
+print("You are " +str(height) + " cm tall")
