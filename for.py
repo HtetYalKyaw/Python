02d5bb7b228694f78cbb37fea5958aa1498b961b
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-04-20 11:09:55 +0630

    commit

---
+++ for.py
@@ -0,0 +1,18 @@
+import time
+
+# while loop = unlimited
+# for loop = limited
+
+# for i in range (10):
+#     print(i+1)
+
+# for i in range (50,100+1):
+#     print(i)
+
+# for i in "Htet Yal Kyaw":
+#     print(i)
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year!")
