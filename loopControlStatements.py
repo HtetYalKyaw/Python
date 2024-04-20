b1157347e4712c93a45b6ff13920c4ca9d33e5a4
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-04-20 16:32:32 +0630

    commit

---
+++ loopControlStatements.py
@@ -0,0 +1,18 @@
+# while True:
+#     name = input("What is Your Name ?")
+#     if name != "":
+#         break
+# print(name)
+
+# Ph_No = "123-456-7890"
+# for i in Ph_No:
+#     if i == "-":
+#         continue
+#     print(i, end="")
+
+for i in range(1,20):
+    if i == 13:
+        pass
+    else:
+        print(i)
+
