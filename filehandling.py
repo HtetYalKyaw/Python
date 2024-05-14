6b0e0ffba88ced263971d524d37e65496f541fd9
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-05-14 11:18:35 +0630

    commit

--- filehandling.py
+++ filehandling.py
@@ -3,11 +3,17 @@
 #  print(x)
 # f.close()
 
-# f = open("myfile.txt", "x")
+f = open("index.py", "x")
 
-f = open("myfile.txt", "a")
-f.write("Hello, i m human")
-f.close()
+# f = open("myfile.txt", "a")
+# f.write("Hello, i m human")
+# f.close()
+#
+# f = open("myfile.txt")
+# print(f.read())
 
-f = open("myfile.txt")
-print(f.read())
+# import os
+# if os.path.exists("myfile.txt"):
+#   os.remove("myfile.txt")
+# else:
+#   print("The file does not exist")
