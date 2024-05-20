50800e232f7be31eafb0d8c6a518a67babe031c9
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-05-20 15:15:31 +0630

    commit

---
+++ string format.py
@@ -0,0 +1,27 @@
+# animal = "cow"
+# item = "fence"
+#
+# print("The {} jumped over the {}".format(animal, item))
+# print("The {1} jumped over the {0}".format(animal, item)) #positional argument
+# print("The {animal} jumped over the {item}".format(animal= "cow", item= "fence")) #keyword argument
+#
+# text = "The {} jumped over the {}."
+# print(text.format(animal, item))
+
+# name = "bro"
+#
+# print("Hello, My Name is {}".format(name))
+# print("Hello, My Name is {name:10}.Nice to meet you".format(name="bro"))
+# print("Hello, My Name is {:<10}.Nice to meet you".format(name))
+# print("Hello, My Name is {:>10}.Nice to meet you".format(name))
+# print("Hello, My Name is {:^10}.Nice to meet you".format(name))
+
+number = 1000
+
+print("The numbers is {:3f}".format(number))
+print("The numbers is {:,}".format(number))
+print("The numbers is {:b}".format(number))
+print("The numbers is {:o}".format(number))
+print("The numbers is {:x}".format(number))
+print("The numbers is {:E}".format(number))
+
