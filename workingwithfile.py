d7eb274dacb216262fe911f42eccfd376dec2a0c
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-05-28 18:20:59 +0630

    commit

--- workingwithfile.py
+++ workingwithfile.py
@@ -1,13 +1,22 @@
-import os
+# import os
+#
+# path = ("C:\\Users\\Dell\\Desktop\\SITE\\folder")
+#
+# if os.path.exists(path):
+#     print("Yes, it exists")
+#     if os.path.isfile(path):
+#         print("It is a file.")
+#     elif os.path.isdir(path):
+#         print("It is a directory.")
+#
+# else:
+#     print("No, it doesnt")
 
-path = ("C:\\Users\\Dell\\Desktop\\SITE\\folder")
 
-if os.path.exists(path):
-    print("Yes, it exists")
-    if os.path.isfile(path):
-        print("It is a file.")
-    elif os.path.isdir(path):
-        print("It is a directory.")
+# with open('test.txt') as file:
+#     print(file.read())
 
-else:
-    print("No, it doesnt")
+text = "\n Have a nice day."
+
+with open('test.txt','a') as file:
+    file.write(text)
