60dc3e022a5a7fc63662a0bab3465174ff2e0115
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-09-07 00:04:32 +0630

    commit

---
+++ index.py
@@ -0,0 +1,9 @@
+name = "Htet Yal Kyaw"
+
+# if (name[].islower()):
+#     name = name.capitalize()
+
+First_name = name[:4].capitalize()
+last_name = name[-4:].capitalize()
+print(First_name)
+print(last_name)
